e85342f94614cdc4cdbbdc7746cf0105331292c3
leezekee <841361034@qq.com>
2024-06-20 01:09:22 +0800

    添加界面 - 排行榜（分页部分未完善）

---
+++ src/components/ScrollArea.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="scroll-container">
+        <slot></slot>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.scroll-container {
+    height: 89vh;
+    width: 100%;
+    overflow: auto;
+    padding: 20px;
+}
+</style>
--- src/views/rank/index.vue
+++ src/views/rank/index.vue
@@ -1,11 +1,25 @@
 <template>
-    <div>
-        Rank
+    <div class="rank-container">
+        <ScrollArea>
+            <RankList :ranklist="[]" :row="rows" :loading="loading" />
+        </ScrollArea>
     </div>
 </template>
 
 <script setup>
+import RankList from '@/components/rank/RankList.vue';
+import ScrollArea from '@/components/ScrollArea.vue';
 
+import { ref } from 'vue';
+
+const rows = ref(30);
+const loading = ref(true);
+const ranklist = ref([]);
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.rank-container {
+    padding: 0 100px;
+    margin-top: 20px;
+}
+</style>
