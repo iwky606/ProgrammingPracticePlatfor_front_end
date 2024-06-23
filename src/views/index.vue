2c9fe213e3938572ca7bbb47256f7fc4154a3c86
leezekee <841361034@qq.com>
2024-06-23 23:20:55 +0800

    添加主页临时样式

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,11 +1,16 @@
 <template>
-    <div>
-        Home
+    <div class="flex col gap-2">
+        <Skeleton class="mb-2"></Skeleton>
+        <Skeleton width="10rem" class="mb-2"></Skeleton>
+        <Skeleton width="5rem" class="mb-2"></Skeleton>
+        <Skeleton height="2rem" class="mb-2"></Skeleton>
+        <Skeleton width="10rem" height="4rem"></Skeleton>
     </div>
 </template>
 
 <script setup>
 import { RouterView } from 'vue-router';
+import Skeleton from 'primevue/skeleton';
 </script>
 
 <style lang="scss" scoped>
